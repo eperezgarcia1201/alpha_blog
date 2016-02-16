c71445715cd0eaa41afa00416aaa8d1f183030e9
Elmer <admin@aprode.org>
2016-02-16 01:31:32 -0600

    Added password authentication for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :articles
-	before_save {email.self = email.downcase}
+	before_save { self.email = email.downcase }
 	validates :username, presence: true, 
 	uniqueness: {case_sensitive: false}, 
 	length:{ minimum:3, maximum:20 }
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
 			  length:{maximum: 105 },
 			  uniqueness:{case_sensitive:false},
 			  format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215054417) do
+ActiveRecord::Schema.define(version: 20160216071254) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160215054417) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
