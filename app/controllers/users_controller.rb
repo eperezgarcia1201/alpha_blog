55e93497f3fc84d9698fc570ab16ad4af8237ba2
Elmer <admin@aprode.org>
2016-02-28 23:50:03 -0600

    Pagination for articles and users and users articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,10 @@ class UsersController < ApplicationController
 	end
 	def show
 		@user = User.find(params[:id])
+		@user_paginate = @user.articles.paginate(page: params[:page], per_page: 2)
 	end
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 5)
 	end
 	private
 	def user_params
