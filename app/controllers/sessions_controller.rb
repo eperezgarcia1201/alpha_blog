d4f0b0cfe2c68a514d1d8170e8577ce6766f4ee2
Elmer <admin@aprode.org>
2016-03-04 00:37:42 -0600

    create and destroy for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,22 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		debugger
-		#render 'new'
+		
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "You were successfully logged in"
+			redirect_to users_path(user)
+		else 
+			flash.now[:danger] = "There was an error with your login information"
+			render 'new'
+		end
 	end
 
 	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have successfully logged out"
+		redirect_to root_path
 	end
 
 end
